7dcd42a2ccb9f5b15fabf67c292c9122fbd44180
Genilton Souza <genilton.souza26@gmail.com>
2020-12-10 15:44:20 -0400

    Added user controller
    Added methods:
    get users (list)
    get user (single entity)

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using DatingApp.DbContexts;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            return user;
+        }
+    }
+}
