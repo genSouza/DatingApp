4e587b3325b6aaf0ca6a6557de34c76a67145f56
Genilton Souza <genilton.souza26@gmail.com>
2020-12-10 02:29:39 -0400

    Added entity framework to project
    Added data context
    Modified Startup.cs to added database service.

---
+++ DatingApp/DbContexts/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.DbContexts
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
